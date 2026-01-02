e6790a551fc60940c40765820e272037f8a0e827
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-01-02 18:22:04 +0000

    🫦

---
+++ woset.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# General stuff setip
+
+# Error handling
+set -euo pipefail
+
+# Colors
+RED=$'\e[31m'
+GREEN=$'\e[32m'
+YELLOW=$'\e[33m'
+BLUE=$'\e[34m'
+PURPLE=$'\e[35m'
+CYAN=$'\e[36m'
+WHITE=$'\e[37m'
+NC=$'\e[0m' # No Color
+
+# Actual Commands section
+
+# Array definition
+
+declare -a CMDS=(
+    "curl ipinfo.io | jq"
+    "go install github.com/charmbracelet/mods@latest" # Mods Panty
+)
+CMDSE=${CMDS[1]}
+
+m1() {
+    echo -e "${CYAN}---START---${NC}"
+    echo -e "${BLUE}Executing: $CMDSE ${NC}"
+    eval "$CMDSE"
+    echo -e "${RED}---END---${NC}"
+}
+
+# Execution ZOne
+m1
