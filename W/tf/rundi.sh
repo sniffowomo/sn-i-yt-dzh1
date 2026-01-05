d1b165fe4f0522633ffcd1f98e4a5849be86a900
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-01-05 23:36:17 +0000

    🫦

---
+++ W/tf/rundi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# TF related commands dump
+
+set -euo pipefail
+
+# --- Colors ---
+BBLACK='\033[1;90m'
+BRED='\033[1;91m'
+BGREEN='\033[1;92m'
+BYELLOW='\033[1;93m'
+BBLUE='\033[1;94m'
+BMAGENTA='\033[1;95m'
+BCYAN='\033[1;96m'
+BWHITE='\033[1;97m'
+RESET='\033[0m'
+
+# --- Commands ---
+
+# -- Execution ---
