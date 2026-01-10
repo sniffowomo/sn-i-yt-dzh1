345f046475eab8d01cdd522cac8d701bc4d2a606
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-01-10 23:40:44 +0000

    🫦

--- W/tf/rundi.sh
+++ W/tf/rundi.sh
@@ -15,5 +15,26 @@ BWHITE='\033[1;97m'
 RESET='\033[0m'
 
 # --- Commands ---
+# All comands will be in an array and executed as needed
+t1() {
+    declare -a CMD=(
+        # Installing terraform from manual
+
+    )
+    CMDEXEC="${CMD[0]}"
+    echo -e "${BBLUE}Executing:${RESET} ${CMDEXEC}"
+    eval "${CMDEXEC}"
+    echo -e "${BGREEN}Done!${RESET}"
+}
+
+# Initial group command executed
+t2() {
+    echo -e "${BBLUE}--- Installing Terraform ---${RESET}"
+    wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update && sudo apt install terraform
+    echo -e "${BGREEN}---Terraform installed successfully!---${RESET}"
+}
 
 # -- Execution ---
+t2
