40065d6750a7f564f386cd222099ce6dcafdd71a
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2026-01-14 22:54:45 +0000

    🫦

---
+++ W/dvj/pro.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# General stuff setip
+
+# Error handling
+set -euo pipefail
+
+# Colors
+RED=$'\e[31m'
+GREEN=$'\e[32m'
+YELLOW=$'\e[33m'
+BLUE=$'\e[34m'
+PURPLE=$'\e[35m'
+CYAN=$'\e[36m'
+WHITE=$'\e[37m'
+NC=$'\e[0m' # No Color
+
+# Actual Commands section
+
+# Array definition
+
+declare -a CMDS=(
+    "code --list-extensions"
+)
+CMDSE=${CMDS[0]}
+
+m1() {
+    echo -e "${CYAN}---START---${NC}"
+    echo -e "${BLUE}Executing: $CMDSE ${NC}"
+    eval "$CMDSE"
+    echo -e "${RED}---END---${NC}"
+}
+
+# Execution ZOne
+m1
